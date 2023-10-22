c13b667cfc16772071c0b8a964fc8623cc9ccf48
ploidy <147208413+ploidy@users.noreply.github.com>
2023-10-22 21:11:13 +0100

    WebGL Builds
    WebGL builds

---
+++ Laboratory 1/Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,101 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void FollowPlayerX::Start()
+extern void FollowPlayerX_Start_mE73AEAF1AE3395058DDD44681F9CC519DD2692F6 (void);
+// 0x00000002 System.Void FollowPlayerX::Update()
+extern void FollowPlayerX_Update_m657B1EB411925E30C5169B39F524D340503E0F26 (void);
+// 0x00000003 System.Void FollowPlayerX::.ctor()
+extern void FollowPlayerX__ctor_m95820EBAEB39F06210CD082F86E5B37AFDCB3ECE (void);
+// 0x00000004 System.Void PlayerControllerX::Start()
+extern void PlayerControllerX_Start_m5BAE410291E1DABAC99D3F233D14AF0AA072A606 (void);
+// 0x00000005 System.Void PlayerControllerX::FixedUpdate()
+extern void PlayerControllerX_FixedUpdate_mF6B5972ED4C7FA460F293640E0EF7A5DDE586A23 (void);
+// 0x00000006 System.Void PlayerControllerX::.ctor()
+extern void PlayerControllerX__ctor_m3FFA731641AD6FA1CE627820042B7587E98D535A (void);
+// 0x00000007 System.Void FollowPlayer::Start()
+extern void FollowPlayer_Start_m138697E0740DB2570074B2DB69AE32AF46A994F9 (void);
+// 0x00000008 System.Void FollowPlayer::LateUpdate()
+extern void FollowPlayer_LateUpdate_m69B79F77CFC8072526CE0BA2D71179F66AE15B70 (void);
+// 0x00000009 System.Void FollowPlayer::.ctor()
+extern void FollowPlayer__ctor_mFEF642F6D296A2FBA30A01B637B6BD8DD2446114 (void);
+// 0x0000000A System.Void OncomingTraffic::Start()
+extern void OncomingTraffic_Start_m8115278E25FBA37CFD0694C1AB4459BC603D8776 (void);
+// 0x0000000B System.Void OncomingTraffic::Update()
+extern void OncomingTraffic_Update_mF61B57A43CDB317590448E30C2553B023749CDB2 (void);
+// 0x0000000C System.Void OncomingTraffic::.ctor()
+extern void OncomingTraffic__ctor_m2538AA4EDEDB6822627544A376B1E3E0A51743D6 (void);
+// 0x0000000D System.Void PlayerController::Start()
+extern void PlayerController_Start_m1D83076E8B136A71051F2F02545EE04947D3A8CF (void);
+// 0x0000000E System.Void PlayerController::Update()
+extern void PlayerController_Update_m1F4051EB5BCBCCE5EEE2E3E49B7E278C3B14EC33 (void);
+// 0x0000000F System.Void PlayerController::.ctor()
+extern void PlayerController__ctor_mDDAB7C7D82E1A5B3E6C197B1AB9D653DFE554F33 (void);
+static Il2CppMethodPointer s_methodPointers[15] = 
+{
+	FollowPlayerX_Start_mE73AEAF1AE3395058DDD44681F9CC519DD2692F6,
+	FollowPlayerX_Update_m657B1EB411925E30C5169B39F524D340503E0F26,
+	FollowPlayerX__ctor_m95820EBAEB39F06210CD082F86E5B37AFDCB3ECE,
+	PlayerControllerX_Start_m5BAE410291E1DABAC99D3F233D14AF0AA072A606,
+	PlayerControllerX_FixedUpdate_mF6B5972ED4C7FA460F293640E0EF7A5DDE586A23,
+	PlayerControllerX__ctor_m3FFA731641AD6FA1CE627820042B7587E98D535A,
+	FollowPlayer_Start_m138697E0740DB2570074B2DB69AE32AF46A994F9,
+	FollowPlayer_LateUpdate_m69B79F77CFC8072526CE0BA2D71179F66AE15B70,
+	FollowPlayer__ctor_mFEF642F6D296A2FBA30A01B637B6BD8DD2446114,
+	OncomingTraffic_Start_m8115278E25FBA37CFD0694C1AB4459BC603D8776,
+	OncomingTraffic_Update_mF61B57A43CDB317590448E30C2553B023749CDB2,
+	OncomingTraffic__ctor_m2538AA4EDEDB6822627544A376B1E3E0A51743D6,
+	PlayerController_Start_m1D83076E8B136A71051F2F02545EE04947D3A8CF,
+	PlayerController_Update_m1F4051EB5BCBCCE5EEE2E3E49B7E278C3B14EC33,
+	PlayerController__ctor_mDDAB7C7D82E1A5B3E6C197B1AB9D653DFE554F33,
+};
+static const int32_t s_InvokerIndices[15] = 
+{
+	985,
+	985,
+	985,
+	985,
+	985,
+	985,
+	985,
+	985,
+	985,
+	985,
+	985,
+	985,
+	985,
+	985,
+	985,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	15,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
---
+++ Laboratory 1/Library/Bee/artifacts/emscripten/2gf01/wrapper_emar.bat
@@ -0,0 +1,4 @@
+
+@echo off
+"E:\Unity\2021.3.30f1\Editor\Data\PlaybackEngines\WebGLSupport\BuildTools\Emscripten\emscripten\emar.bat" %* < nul
+exit %ERRORLEVEL%
