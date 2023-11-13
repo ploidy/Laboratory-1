71e9caafc4ff35b3327d40db550b611f1326a763
ploidy <147208413+ploidy@users.noreply.github.com>
2023-11-13 00:25:29 +0000

    Week08Lesson6.2
    CenterOfMass

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour // player controller inherits from MonoBehaviour
 {
     //Private Variables
-    [SerializeField] float speed = 20.0f;
+    //[SerializeField] float speed = 20.0f;
+    [SerializeField] private float horsePower = 0;
     [SerializeField] float turnSpeed = 45.0f;
     private float horizontalInput;
     private float forwardInput;
+    private Rigidbody playerRb;
+    [SerializeField] GameObject centerOfMass;
     
     //Public Variables
     public string inputID;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.centerOfMass = centerOfMass.transform.localPosition;
+    }
 
     // Update is called once per frame
     void FixedUpdate()
@@ -20,7 +29,8 @@ public class PlayerController : MonoBehaviour // player controller inherits from
         horizontalInput = Input.GetAxis("Horizontal" + inputID);
         forwardInput = Input.GetAxis ("Vertical" + inputID);
         // Move the vehicle forward based on vertical input
-        transform.Translate (Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // transform.Translate (Vector3.forward * Time.deltaTime * speed * forwardInput);
+        playerRb.AddRelativeForce(Vector3.forward * horsePower * forwardInput);
         // Rotates the car based on horizontal input
         transform.Rotate (Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
